45402f49aa3d7fb32e9d9e946df61ed7050c3432
InacioMugana <inaciomugaua@gmail.com>
2024-07-29 16:59:45 +0200

    translate language home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,20 +9,20 @@ export const Home = () => {
       <div className="bg-gray-100 min-h-screen flex items-center justify-center">
         <div className="bg-white p-8 rounded-lg shadow-lg">
           <h1 className="text-3xl font-bold mb-4">
-            Bem-vindo à pagina de gestão de estudante
+            Welcome to the student management page
           </h1>
           <p className="text-gray-700 mb-4">
-            Utilizamos tecnologias da informação e comunicação para melhorar a
-            gestão e a experiência de aprendizagem, como sistemas de gestão
-            escolar (SGEs), plataformas de ensino à distância (EAD) e
-            ferramentas de análise de dados educacionais.
+            We use information and communication technologies to improve
+            management and the learning experience, as management systems
+            (SGEs), distance learning platforms (EAD) and educational data
+            analysis tools.
           </p>
           <p>
             <Link
               to={"/login"}
               className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
             >
-              Entrar
+              Enter
             </Link>
           </p>
         </div>
